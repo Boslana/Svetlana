73d378ee379f9286297a6f354aca35e95999cc09
Svetlana Polorotova <boslana25@gmail.com>
2022-01-20 13:37:38 +0300

    dz

--- src/test/java/Main/BaseClass.kt
+++ src/test/java/Main/BaseClass.kt
@@ -23,19 +23,18 @@ class BaseClass {
         val caps = DesiredCapabilities()
 
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android") // название платформы
-        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0") // версия ОС
-        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 4a API 30") // имя устройства
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11") // версия ОС
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Galaxy A50") // имя устройства
         caps.setCapability(MobileCapabilityType.APP, "/Users/handh/sportmaster-4.0.13.5605_dev_beta.apk")
         caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "ru.sportmaster.app.handh.dev")
         caps.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "ru.sportmaster.app.presentation.start.StartActivity")
-        caps.setCapability(MobileCapabilityType.NO_RESET, true) // не сбрасывать приложение в 0 перед новым запуском
+        caps.setCapability(MobileCapabilityType.NO_RESET, false) // не сбрасывать приложение в 0 перед новым запуском
         caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "7200")
-        // cap.setCapability(MobileCapabilityType.UDID, "")
+        caps.setCapability(MobileCapabilityType.UDID, "R58M67DH10R")
 
         driver = AndroidDriver(url, caps) // установка драйвера и приложения на Android устройство
 
-        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS)
-
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS)
 
     }
 
@@ -43,7 +42,6 @@ class BaseClass {
     fun end() {
         driver.quit()
     }
-
 
     @Test
     fun testOne() {
@@ -59,16 +57,38 @@ class BaseClass {
             println("Элемент не найден, тест продолжается")
         }
 
-
         //Ввод текста в поле
         lateinit var element2: MobileElement //создаем объект MobileElement
         element2 = driver.findElement(MobileBy.id("ru.sportmaster.app.handh.dev:id/editTextPhone")) //производим поиск элемента по локатору id
-        element2.sendKeys("9999999905") //ввод текста в поле
+        element2.sendKeys("9999999988") // ввод текста в поле
 
         // клик на кнопку получить код
-        lateinit var element3: MobileElement //создаем объект MobileElement
+        lateinit var element3: MobileElement // создаем объект MobileElement
         element3 = driver.findElement(MobileBy.id("ru.sportmaster.app.handh.dev:id/buttonGetCode")) //производим поиск элемента по локатору id
         element3.click() //клик
+
+        //Ввод полученного кода
+        lateinit var element4: MobileElement
+        element4 = driver.findElement(MobileBy.id("ru.sportmaster.app.handh.dev:id/pinCodeEditText")) //производим поиск элемента по локатору id
+        element4.sendKeys("1111") // ввод кода
+        println("ввод кода-успешно")
+
+        // даем доступ к геолокации
+        lateinit var element5: MobileElement
+        element5 = driver.findElement(MobileBy.id("com.android.permissioncontroller:id/permission_allow_foreground_only_button"))
+        element5.click() // клик по элементу
+
+        // выбираем город
+        lateinit var element6: MobileElement
+        element6 = driver.findElement(MobileBy.id("android:id/button1"))
+        element6.click()
+
+        // переходим в профиль
+        lateinit var element7: MobileElement
+        element7 = driver.findElement(MobileBy.id("ru.sportmaster.app.handh.dev:id/profile_graph"))
+        element7.click()
+
+
 
         TimeUnit.SECONDS.sleep(5)
 
